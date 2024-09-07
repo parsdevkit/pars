94175a1de745842ce3772027708d97c8d9c8d167
Ahmet Soner <ahmettsoner@gmail.com>
2024-09-07 20:57:22 +0300

    Makefile added

--- makefiles/modules/build/bin/common.mk
+++ makefiles/modules/build/bin/common.mk
@@ -5,4 +5,8 @@ build.binary: $(SOURCE_ROOT_DIR)/pars.go $(SOURCE_ROOT_DIR)/go.mod
 	@echo "Building binaries for $(APP_OS) $(APP_ARCH) on $(SOURCE_ROOT_DIR)"
 	GOOS=$(APP_OS) GOARCH=$(APP_ARCH) cd $(SOURCE_ROOT_DIR) && go build -o ../$(BIN_ARTIFACTS_DIR)/$(APP) pars.go
 
+build.binary.vendor: $(SOURCE_ROOT_DIR)/pars.go $(SOURCE_ROOT_DIR)/go.mod
+	@echo "Building binaries for $(APP_OS) $(APP_ARCH) on $(SOURCE_ROOT_DIR)"
+	GOOS=$(APP_OS) GOARCH=$(APP_ARCH) GOFLAGS=-mod=vendor cd $(SOURCE_ROOT_DIR) && go build -o ../$(BIN_ARTIFACTS_DIR)/$(APP) pars.go
+
 
--- makefiles/modules/build/bin/windows.mk
+++ makefiles/modules/build/bin/windows.mk
@@ -2,3 +2,6 @@ include ./makefiles/modules/build/bin/common.mk
 
 build.binary.windows: 
 	@$(MAKE) build.binary OS=$(OS_WINDOWS) ARCH=$(ARCH)
+
+build.binary.windows.vendor:
+	@$(MAKE) build.binary.vendor OS=$(OS_WINDOWS) ARCH=$(ARCH)
