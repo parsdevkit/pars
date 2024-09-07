82fd370e9438df95318d890a83b8855c58f53e91
Ahmet Soner <ahmettsoner@gmail.com>
2024-09-07 21:33:06 +0300

    Makefile added

--- makefiles/modules/build/bin/common.mk
+++ makefiles/modules/build/bin/common.mk
@@ -3,11 +3,11 @@ include ./makefiles/init.mk
 
 build.binary: $(SOURCE_ROOT_DIR)/pars.go $(SOURCE_ROOT_DIR)/go.mod
 	@echo "Building binaries for $(APP_OS) $(APP_ARCH) on $(SOURCE_ROOT_DIR)"
-	GOOS=$(APP_OS) GOARCH=$(APP_ARCH) cd $(SOURCE_ROOT_DIR) && go build -o ../$(BIN_ARTIFACTS_DIR)/$(APP) pars.go
+	cd $(SOURCE_ROOT_DIR) && GOOS=$(APP_OS) GOARCH=$(APP_ARCH) go build -ldflags="-X 'parsdevkit.net/core/utils.version=$(APP_TAG)' -buildid=$(APPLICATION_NAME)" -o ../$(BIN_ARTIFACTS_DIR)/$(APP) pars.go
 
 build.binary.vendor: $(SOURCE_ROOT_DIR)/pars.go $(SOURCE_ROOT_DIR)/go.mod
 	@echo "Building binaries for $(APP_OS) $(APP_ARCH) on $(SOURCE_ROOT_DIR)"
 	mkdir -p $(TMP_DIR)/.gocache $(TMP_DIR)/.gomodcache
-	GOOS=$(APP_OS) GOARCH=$(APP_ARCH) GOFLAGS=-mod=vendor GOCACHE=$(TMP_DIR)/.gocache GOMODCACHE=$(TMP_DIR)/.gomodcache cd $(SOURCE_ROOT_DIR) && go build -o ../$(BIN_ARTIFACTS_DIR)/$(APP) pars.go
+	cd $(SOURCE_ROOT_DIR) && GOCACHE=$(TMP_DIR)/.gocache GOMODCACHE=$(TMP_DIR)/.gomodcache GOFLAGS=-mod=vendor GOOS=$(APP_OS) GOARCH=$(APP_ARCH) go build -ldflags="-X 'parsdevkit.net/core/utils.version=$(APP_TAG)' -buildid=$(APPLICATION_NAME)" -o ../$(BIN_ARTIFACTS_DIR)/$(APP) pars.go
 
 
