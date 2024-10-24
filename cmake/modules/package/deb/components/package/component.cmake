cdba5ef8ff9a77621416ab630b3842954b46fdef
Ahmet Soner <ahmettsoner@gmail.com>
2024-10-24 07:24:31 +0300

    deb module improved

--- cmake/modules/package/deb/components/package/component.cmake
+++ cmake/modules/package/deb/components/package/component.cmake
@@ -1,19 +1,19 @@
 foreach(DEBARCH ${DEBARCH_LIST_LINUX})
     map_debarch_to_arch(${DEBARCH} APP_ARCH)
     set(DEB_ROOT_DIR ${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/linux/pkg/${APP_ARCH})
-    set(DEB_PAYLOAD_DIR ${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/linux/pkg/${APP_ARCH}/${APP_NAME})
-    set(DEB_OUTPUT_DIR ${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/linux/pkg/${APP_ARCH}/output)
+    set(DEB_PAYLOAD_DIR ${DEB_ROOT_DIR}/${APP_NAME})
+    set(DEB_OUTPUT_DIR ${DEB_ROOT_DIR}/output)
 
     add_custom_command(
         OUTPUT ${DEB_OUTPUT_DIR}
         COMMAND ${CMAKE_COMMAND} -E echo "Building source files."
-        COMMAND chmod +w ${DEB_PAYLOAD_DIR} && cd ${DEB_PAYLOAD_DIR} && dpkg-buildpackage -S
-        COMMAND mkdir ${DEB_OUTPUT_DIR}
-        COMMAND $(DEB_ROOT_DIR)/$(APP_NAME)_* $(DEB_OUTPUT_DIR)
+        COMMAND cd ${DEB_PAYLOAD_DIR} && dpkg-buildpackage -S
+        COMMAND mkdir -p ${DEB_OUTPUT_DIR}
+        COMMAND mv ${DEB_ROOT_DIR}/${APP_NAME}_* ${DEB_OUTPUT_DIR}/
         COMMENT "Building .deb package"
     )
 
-    add_custom_target(build.deb.package.${APP_ARCH}.package ALL DEPENDS check_env_for_deb_packing ${DEB_OUTPUT_DIR})
+    add_custom_target(build.deb.package.${APP_ARCH}.package DEPENDS check_env_for_deb_packing ${DEB_OUTPUT_DIR})
 
     add_custom_target(build.deb.package.${APP_ARCH})
     add_dependencies(build.deb.package.${APP_ARCH} 
@@ -23,3 +23,28 @@ foreach(DEBARCH ${DEBARCH_LIST_LINUX})
         build.deb.package.${APP_ARCH}.package
     )
 endforeach()
+
+
+set(DEBARCH any)
+set(DEB_ROOT_DIR ${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/linux/pkg/all)
+set(DEB_PAYLOAD_DIR ${DEB_ROOT_DIR}/${APP_NAME})
+set(DEB_OUTPUT_DIR ${DEB_ROOT_DIR}/output)
+
+add_custom_command(
+    OUTPUT ${DEB_OUTPUT_DIR}
+    COMMAND ${CMAKE_COMMAND} -E echo "Building source files."
+    COMMAND cd ${DEB_PAYLOAD_DIR} && dpkg-buildpackage -S
+    COMMAND mkdir -p ${DEB_OUTPUT_DIR}
+    COMMAND mv ${DEB_ROOT_DIR}/${APP_NAME}_* ${DEB_OUTPUT_DIR}/
+    COMMENT "Building .deb package"
+)
+
+add_custom_target(build.deb.package.all.package DEPENDS check_env_for_deb_packing ${DEB_OUTPUT_DIR})
+
+add_custom_target(build.deb.package.all)
+add_dependencies(build.deb.package.all 
+    # build.deb.package.setup
+    build.deb.package.all.configuration
+    build.deb.package.all.payload
+    build.deb.package.all.package
+)
