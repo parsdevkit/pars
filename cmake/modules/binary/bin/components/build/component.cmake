6a67fafd45749ca8cd59951a01d093a77f9095b2
Ahmet Soner <ahmettsoner@gmail.com>
2024-11-09 08:07:11 +0300

    cmake build updated and versioning improved

--- cmake/modules/binary/bin/components/build/component.cmake
+++ cmake/modules/binary/bin/components/build/component.cmake
@@ -10,35 +10,21 @@ foreach(GOOS ${GOOS_LIST})
     foreach(GOARCH ${ARCH_LIST})
         map_goarch_to_arch(${GOARCH} APP_ARCH)
         
-        set_build_output_from_arg("${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/${APP_ARCH}/${APP_NAME}${EXT}" "${GOOS}_${APP_ARCH}_${APP_TAG}_${APP_NAME}${EXT}" BUILD_OUTPUT_PATH)
-        build("${GOOS}" "${GOARCH}" "${EXT}" OFF "${BUILD_OUTPUT_PATH}")
+        set(PATH_OUTPUT ${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/${APP_ARCH}/${APP_NAME}${EXT})
+        set_build_output_from_arg(BUILD_OUTPUT_PATH)
+        build("${GOOS}" "${GOARCH}" "${BUILD_OUTPUT_PATH}/${APP_NAME}${EXT}")
         add_custom_target(build.binary.${GOOS}.${APP_ARCH}
-            DEPENDS ${BUILD_OUTPUT_PATH}
+            DEPENDS ${PATH_OUTPUT}
         )
         list(APPEND OS_ALL_TARGETS "build.binary.${GOOS}.${APP_ARCH}")
         list(APPEND ALL_TARGETS "build.binary.${GOOS}.${APP_ARCH}")
-
-        
-        set_build_output_from_arg("${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/v_${APP_ARCH}/${APP_NAME}${EXT}" "v_${GOOS}_${APP_ARCH}_${APP_TAG}_${APP_NAME}${EXT}" BUILD_OUTPUT_PATH)
-        build("${GOOS}" "${GOARCH}" "${EXT}" ON "${BUILD_OUTPUT_PATH}")
-        add_custom_target(build.binary.vendor.${GOOS}.${APP_ARCH}
-            DEPENDS ${BUILD_OUTPUT_PATH}
-        )
-        list(APPEND OS_ALL_TARGETS_VENDOR "build.binary.vendor.${GOOS}.${APP_ARCH}")
-        list(APPEND ALL_TARGETS_VENDOR "build.binary.vendor.${GOOS}.${APP_ARCH}")
     endforeach()
         add_custom_target(build.binary.${GOOS}.all
             DEPENDS ${OS_ALL_TARGETS}
         )
-        add_custom_target(build.binary.vendor.${GOOS}.all
-            DEPENDS ${OS_ALL_TARGETS_VENDOR}
-        )
 endforeach()
 add_custom_target(build.binary.all
     DEPENDS ${ALL_TARGETS}
-)
-add_custom_target(build.binary.vendor.all
-    DEPENDS ${ALL_TARGETS_VENDOR}
 )
 
 
@@ -50,31 +36,21 @@ get_host_arch(APP_ARCH)
 map_arch_to_goarch(${APP_ARCH} GOARCH)
 
 
-set_build_output_from_arg("${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/${APP_ARCH}/${APP_NAME}${EXT}" "${GOOS}_${APP_ARCH}_${APP_TAG}_${APP_NAME}${EXT}" BUILD_OUTPUT_PATH)
-build("${GOOS}" "${GOARCH}" "${EXT}" OFF "${BUILD_OUTPUT_PATH}")
+set(PATH_OUTPUT ${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/${APP_ARCH}/${APP_NAME}${EXT})
+set_build_output_from_arg(BUILD_OUTPUT_PATH)
+build("${GOOS}" "${GOARCH}" "${BUILD_OUTPUT_PATH}/${APP_NAME}${EXT}")
 add_custom_target(build.binary
-    DEPENDS ${BUILD_OUTPUT_PATH}
-)
-
-set_build_output_from_arg("${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/v_${APP_ARCH}/${APP_NAME}${EXT}" "v_${GOOS}_${APP_ARCH}_${APP_TAG}_${APP_NAME}${EXT}" BUILD_OUTPUT_PATH)
-build("${GOOS}" "${GOARCH}" "${EXT}" ON "${BUILD_OUTPUT_PATH}")
-add_custom_target(build.binary.vendor
-    DEPENDS ${BUILD_OUTPUT_PATH}
+    DEPENDS ${PATH_OUTPUT}
 )
 
 set_goos_arch_lists(${GOOS})
 foreach(GOARCH ${ARCH_LIST})
     map_goarch_to_arch(${GOARCH} APP_ARCH)
 
-    set_build_output_from_arg("${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/${APP_ARCH}/${APP_NAME}${EXT}" "${GOOS}_${APP_ARCH}_${APP_TAG}_${APP_NAME}${EXT}" BUILD_OUTPUT_PATH)
-    build("${GOOS}" "${GOARCH}" "${EXT}" OFF "${BUILD_OUTPUT_PATH}")
+    set(PATH_OUTPUT ${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/${APP_ARCH}/${APP_NAME}${EXT})
+    set_build_output_from_arg(BUILD_OUTPUT_PATH)
+    build("${GOOS}" "${GOARCH}" "${BUILD_OUTPUT_PATH}/${APP_NAME}${EXT}")
     add_custom_target(build.binary.${APP_ARCH}
-        DEPENDS ${BUILD_OUTPUT_PATH}
-    )
-
-    set_build_output_from_arg("${CMAKE_SOURCE_DIR}/${DIST_ROOT_DIR}/${APP_TAG}/${GOOS}/bin/v_${APP_ARCH}/${APP_NAME}${EXT}" "v_${GOOS}_${APP_ARCH}_${APP_TAG}_${APP_NAME}${EXT}" BUILD_OUTPUT_PATH)
-    build("${GOOS}" "${GOARCH}" "${EXT}" ON "${BUILD_OUTPUT_PATH}")
-    add_custom_target(build.binary.vendor.${APP_ARCH}
-        DEPENDS ${BUILD_OUTPUT_PATH}
+        DEPENDS ${PATH_OUTPUT}
     )
 endforeach()
