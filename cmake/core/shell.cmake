045829459536c598a5294932d10aca82c04c03d8
Ahmet Soner <ahmettsoner@gmail.com>
2024-11-16 11:52:03 +0300

    cmake shell support improved #304

--- cmake/core/shell.cmake
+++ cmake/core/shell.cmake
@@ -63,22 +63,62 @@ endfunction()
 get_shell(HOST_SHELL)
 
 
+function(env_command_for_shell shell env_vars output_variable_name)
+    get_filename_component(shell_name ${shell} NAME)
+
+    # Set prefix based on shell type
+    if(${shell_name} STREQUAL "powershell" OR ${shell_name} STREQUAL "pwsh")
+        set(prefix "$env:")
+        set(separator " ; ")  # Use ; as separator for PowerShell
+    elseif(${shell_name} STREQUAL "cmd")
+        set(prefix "set ")
+        set(separator " & ")  # Use & as separator for CMD
+    elseif(${shell_name} STREQUAL "bash" OR ${shell_name} STREQUAL "zsh" OR ${shell_name} STREQUAL "fish")
+        set(prefix "export ")
+        set(separator " && ")  # Use && as separator for bash-like shells
+    else()
+        message(FATAL_ERROR "Unsupported shell: ${shell_name}")
+    endif()
+
+    # Prepare environment variable commands
+    set(env_commands "")
+    foreach(env_var IN LISTS env_vars)
+        if(${shell_name} STREQUAL "powershell" OR ${shell_name} STREQUAL "pwsh")
+            string(REPLACE "=" "=\"" env_var_quoted ${env_var})
+            set(env_var_quoted "${env_var_quoted}\"")
+            list(APPEND env_commands "${prefix}${env_var_quoted}")
+            elseif(${shell_name} STREQUAL "cmd")
+            list(APPEND env_commands "${prefix}${env_var}")
+        else()  # For bash, zsh, fish
+            list(APPEND env_commands "${prefix}${env_var}")
+        endif()
+    endforeach()
+
+    # Join all commands with the appropriate separator
+    string(JOIN "${separator}" final_command ${env_commands})
+
+    # Set the joined command string in the output variable
+    set(${output_variable_name} "${final_command}" PARENT_SCOPE)
+endfunction()
+
 
 function(command_for_shell shell command_list output_variable_name)
     get_filename_component(shell_name ${shell} NAME)
 
     
     if(${shell_name} STREQUAL "powershell" OR ${shell_name} STREQUAL "pwsh")
-        set(separator " \\\\\\\; ")
+        set(separator " \\\\\\; ")
+    elseif(${shell_name} STREQUAL "cmd")
+        set(separator " & ")
     else()
         set(separator " && ")
     endif()
 
     string(JOIN "${separator}" command_to_run ${command_list})
 
-    if(${shell_name} STREQUAL "${HOST_SHELL}") 
-        set(shell_command ${command_to_run})
-    elseif(${shell_name} STREQUAL "cmd")
+    # if(${shell_name} STREQUAL "${HOST_SHELL}") 
+    #     set(shell_command ${command_to_run})
+    if(${shell_name} STREQUAL "cmd")
         set(shell_command cmd /c ${command_to_run})
     elseif(${shell_name} STREQUAL "powershell")
         set(shell_command powershell -ExecutionPolicy Bypass -Command ${command_to_run})
